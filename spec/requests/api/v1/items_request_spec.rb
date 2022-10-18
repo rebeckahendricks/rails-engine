40b1602d584d4071f15920905dc2df67d6554d2b
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-10-18 15:12:50 -0600

    Create 'destroy an item' test on items_request_spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -115,4 +115,23 @@ describe 'Items API' do
     expect(item.name).to_not eq(previous_name)
     expect(item.name).to eq('Fork')
   end
+
+  it 'can destroy an item' do
+    merchant = create(:merchant)
+
+    item = Item.create!(
+      merchant_id: merchant.id,
+      name: Faker::Lorem.word,
+      description: Faker::Lorem.sentence,
+      unit_price: Faker::Number.decimal(l_digits: 3, r_digits: 2)
+    )
+
+    expect(Item.count).to eq(1)
+
+    delete "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{ Item.find(item.id) }.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
