c8fef953ed6fbb583e753f8d2cc30872987a6cdc
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-10-18 13:20:59 -0600

    Create method in controller, item_params, routes, items create spec

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -7,4 +7,14 @@ class Api::V1::ItemsController < ApplicationController
     item = Item.find(params[:id])
     render json: ItemSerializer.new(item)
   end
+
+  def create
+    render json: ItemSerializer.new(Item.create(item_params)), status: 201
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:merchant_id, :name, :description, :unit_price)
+  end
 end
