c48439775748b69501583aa4c1206d26a07a0300
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-10-18 11:42:35 -0600

    Create fetch one merchant feature

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,20 @@ describe 'Merchants API' do
       expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to be_a(String)
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
 end
